b271a93859100b611d77876c69690e1ecbf84eef
Gabriel Majeri <gabriel.majeri6@gmail.com>
2021-03-21 09:35:21 +0200

    Merge office scene into main game project

--- game/house_scene/House.gd
+++ game/house_scene/House.gd
@@ -19,12 +19,3 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel") and fast_close:
 		get_tree().quit() # Quits the game
-	
-	if event.is_action_pressed("mouse_input") and fast_close:
-		match mouse_mode: # Switch statement in GDScript
-			"CAPTURED":
-				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-				mouse_mode = "VISIBLE"
-			"VISIBLE":
-				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-				mouse_mode = "CAPTURED"
