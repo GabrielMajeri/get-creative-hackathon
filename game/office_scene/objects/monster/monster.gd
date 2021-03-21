5c7294d5d3df9f633058ba0242420cb3dae51de1
mihainsto <mihaistoianro@gmail.com>
2021-03-21 11:32:30 +0200

    Add bunny

--- game/office_scene/objects/monster/monster.gd
+++ game/office_scene/objects/monster/monster.gd
@@ -17,7 +17,6 @@ onready var anim = get_node("AnimationPlayer")
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(hitting):
