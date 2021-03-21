28153b601246f83bd8f4d9673d3fe1f3e97a30ea
Gabriel Majeri <gabriel.majeri6@gmail.com>
2021-03-21 09:55:51 +0200

    Get main menu to work

--- game/main_menu_scene/TitleScreen.gd
+++ game/main_menu_scene/TitleScreen.gd
@@ -2,8 +2,14 @@ extends MarginContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var start_button = $Menu/CenterRow/Buttons/StartGameButton
+	start_button.connect("pressed", self, "_start_button_pressed")
+
 	var quit_button = $Menu/CenterRow/Buttons/QuitGameButton
 	quit_button.connect("pressed", self, "_quit_button_pressed")
+
+func _start_button_pressed():
+	get_tree().change_scene("res://house_scene/House.tscn")
 
 func _quit_button_pressed():
 	# Exit the game
